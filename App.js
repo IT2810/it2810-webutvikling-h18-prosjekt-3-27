76a82d573d229c72f36c1f73e5aa32dc18cfde9a
Helle van den Broek <hvbroek@stud.ntnu.no>
2018-09-26 13:59:26 +0200

    Implementet tab-bar

--- App.js
+++ App.js
@@ -1,15 +1,100 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation';
+import Home from "./components/Home";
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export default class App extends React.Component {
+export class App extends Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Home />
       </View>
     );
   }
 }
+
+
+/*Bare å slette klassene nedenfor, men trengte de for å kunne loade appen og teste tabs*/
+export class Todo extends Component{
+  render() {
+    return (
+      <View>
+        <Text>Test todo</Text>
+      </View>
+    );
+  }
+}
+
+export class Calendar extends Component{
+  render() {
+    return (
+      <View>
+        <Text>test cal</Text>
+      </View>
+    );
+  }
+}
+export class Contacts extends Component{
+  render() {
+    return (
+      <View>
+        <Text>test contacts</Text>
+      </View>
+    );
+  }
+}
+/* -------------------------------------------*/
+
+export default createBottomTabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name = "ios-home" color={tintColor} size = {24} />
+      )
+    }
+  },
+  Todo: {
+    screen: Todo,
+    navigationOptions: {
+      tabBarLabel: 'Todo',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-checkmark-circle" color={tintColor} size={24}/>
+      )
+    }
+  },
+  Calendar: {
+    screen: Calendar,
+    navigationOptions: {
+      tabBarLabel: 'Calendar',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-calendar" color={tintColor} size={24}/>
+      )
+    }
+  },
+  Contacts: {
+    screen: Contacts,
+    navigationOptions: {
+      tabBarLabel: 'Contacts',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-contacts" color={tintColor} size={24}/>
+      )
+    }
+  }
+}, {
+  initialRouteName: 'Home',
+  order: ['Home', 'Contacts', 'Calendar', 'Todo'],
+  navigationOptions : {
+    tabBarVisible: true,
+  },
+  tabBarOptions: {
+    activeTintColor: 'black',
+    inactiveTintColor: 'grey'
+  }
+
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +104,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
