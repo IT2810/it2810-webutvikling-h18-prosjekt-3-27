a0809904276ffc425e5c67ce6f818ba4e5147b99
Sander Breivik <sandeb@stud.ntnu.no>
2018-09-30 14:25:14 +0200

    Added AsyncStorage

--- components/Todo/Todo.js
+++ components/Todo/Todo.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
 import {
+  AsyncStorage,
+  FlatList,
+  KeyboardAvoidingView,
+  ScrollView,
   StyleSheet,
   Text,
-  View,
-  FlatList,
   TextInput,
-  KeyboardAvoidingView,
+  View,
 } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import randomColor from "randomcolor";
@@ -22,15 +24,28 @@ export default class Todo extends Component {
     this._handleDeleteTask = this._handleDeleteTask.bind(this);
   }
 
+  componentDidMount() {
+    Tasks._load(tasks => this.setState({ tasks: tasks || [] }));// this.setState({ tasks: tasks || [] }))
+  }
+
   _handleAddTask = () => {
-    this.setState(
-      prevState => {
-        let {tasks, text} = prevState;
-        return {
-          tasks: tasks.concat({key: tasks.length, text: text, completed: false, color: randomColor({hue: "green"})}),
-          text: ""
-        };
-      });
+    if (this.state.text.length > 0) {
+      this.setState(
+        prevState => {
+          let {tasks, text} = prevState;
+          return {
+            tasks: tasks.concat({
+              key: tasks.length,
+              text: text,
+              completed: false,
+              color: randomColor({luminosity: 'dark', hue: "green"})
+            }),
+            text: ""
+          };
+        },
+        () => Tasks._save(this.state.tasks)
+      );
+    }
   };
 
   _handleDeleteTask = index => {
@@ -41,7 +56,8 @@ export default class Todo extends Component {
         let completedTasks = [...prevState.completedTasks.slice()];
         let completedTask = tasks.splice(index, 1);
         return {tasks: tasks, completedTasks: completedTasks.concat(completedTask)};
-      }
+      },
+      () => Tasks._save(this.state.tasks)
     );
   };
 
@@ -51,28 +67,34 @@ export default class Todo extends Component {
       <View
         style={styles.container}
       >
-        <FlatList
-          style={styles.list}
-          data={this.state.tasks}
-          keyExtractor={(item) => item.key.toString()}
-          renderItem={({item, index}) =>
-            <View>
-              <View style={styles.itemContainer}>
-                <Text style={styles.item}>
-                  {item.text}
-                </Text>
-                <Icon style={styles.button} name="ios-checkmark-circle" color={item.color} size={50} onPress={() => this._handleDeleteTask(index)}/>
-              </View>
-              <View style={styles.hr}/>
-            </View>}
-        />
+        <ScrollView>
+          <FlatList
+            style={styles.list}
+            data={this.state.tasks}
+            keyExtractor={(item) => item.key.toString()}
+            renderItem={({item, index}) =>
+              <View>
+                <View style={styles.itemContainer}>
+                  <Text style={styles.item}>
+                    {item.text}
+                  </Text>
+                  <Icon style={styles.button} name="ios-checkmark-circle" color={item.color} size={50}
+                        onPress={() => this._handleDeleteTask(index)}/>
+                </View>
+                <View style={styles.hr}/>
+              </View>}
+          />
+        </ScrollView>
+
         <KeyboardAvoidingView behavior="position">
           <Text
             style={{textAlign: "center"}}>{this.state.completedTasks.length}/{this.state.tasks.length + this.state.completedTasks.length}</Text>
           <TextInput
+            ref="input"
             style={styles.textInput}
             onChangeText={text => this.setState({text: text})}
             onSubmitEditing={this._handleAddTask}
+            blurOnSubmit={false}
             value={this.state.text}
             placeholder="Add task.."
             autoCorrect={false}
@@ -82,12 +104,35 @@ export default class Todo extends Component {
       </View>
     );
   }
+
+
 }
+
+let Tasks = {
+  convertToArrayOfObject(tasks, callback) {
+    return callback(
+      tasks ? JSON.parse(tasks).map((task) => ({
+        key: task.key,
+        text: task.text,
+        completed: task.completed,
+        color: task.color
+      })) : []
+    );
+  },
+  _load(callback) {
+    return AsyncStorage.getItem("TASKS", (err, tasks) =>
+      this.convertToArrayOfObject(tasks, callback)
+    );
+  },
+  _save(tasks) {
+    AsyncStorage.setItem("TASKS", JSON.stringify(tasks));
+  }
+};
+
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 20,
-    width: "100%",
     height: "100%",
   },
   itemContainer: {
@@ -96,6 +141,8 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   item: {
+    flex: 1,
+    flexWrap: "wrap",
     paddingLeft: 10,
     paddingTop: 2,
     paddingBottom: 2,
