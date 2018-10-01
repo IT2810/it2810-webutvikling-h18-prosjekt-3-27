b20b4e176f22406c622e56e131cf7e42335a4927
Artorp <Artorp@users.noreply.github.com>
2018-10-01 13:51:11 +0200

    Add docs to setupTests

--- setupTests.js
+++ setupTests.js
@@ -1,4 +1,12 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16.3';
 
+/*
+* See: https://github.com/airbnb/enzyme/issues/1501
+* * Needs react-dom
+* * recommended to use react-native-mock
+*
+* Might add react-native-mock/mock require later
+* */
+
 Enzyme.configure({ adapter: new Adapter() });
