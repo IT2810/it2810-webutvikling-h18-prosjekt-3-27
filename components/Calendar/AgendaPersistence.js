acbd28eda83fbba75c880bdef5295fd7ef4793bd
Artorp <Artorp@users.noreply.github.com>
2018-10-08 19:50:52 +0200

    Begin work on calendar agenda persistence

---
+++ components/Calendar/AgendaPersistence.js
@@ -0,0 +1,23 @@
+import { AsyncStorage } from "react-native";
+
+export default class AgendaPersistence {
+
+  static getAllItems() {
+    /*
+     * returns an object where each key is a date, and its value
+     * is an array of objects where each object is an item for that day.
+     *
+     * Example:
+     * {'2012-05-22': [{text: 'item 1 - any js object'}],
+     *  '2012-05-23': [{text: 'item 2 - any js object'}],
+     *  '2012-05-24': [],
+     *  '2012-05-25': [{text: 'item 3 - any js object'},{text: 'any js object'}],
+     * }
+     *
+     * TODO: Actually do this
+     */
+
+    return {};
+  }
+}
+
--- components/Calendar/CalendarScreen.js
+++ components/Calendar/CalendarScreen.js
@@ -4,13 +4,96 @@ import {
   Text,
   StyleSheet
 } from "react-native";
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Agenda } from 'react-native-calendars';
+import AgendaPersistence from "./AgendaPersistence";
 
 class CalendarScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: {}
+    };
+  }
+
+  componentDidMount() {
+    const d = new Date("2018-10-01");
+    this.loadItems({
+      dateString: d.dateString,
+      day: d.getDay(),
+      month: d.getMonth() + 1,
+      timestamp: d.getTime(),
+      year: d.getFullYear()
+    });
+    this.loadAgenda();
+  }
+
+  async loadAgenda() {
+    const items = await AgendaPersistence.getAllItems();
+    // TODO: set state
+  };
+
+
   render() {
     return (
-      <CalendarList />
+      <Agenda
+        style={{minHeight: 200}}
+        items={this.state.items}
+        minDate={'2018-08-01'}
+        maxDate={'2018-12-31'}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+        firstDay={1}
+      />
     );
+  }
+
+  loadItems(day) {
+    console.log(day);
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'Item for ' + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      // console.log(this.state.items);
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 400);
+    console.log(`Load Items for ${day.dateString}`);
+  }
+
+  renderItem(item) {
+    return (
+      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
   }
 }
 
@@ -20,5 +103,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
+  },
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
   }
 });
