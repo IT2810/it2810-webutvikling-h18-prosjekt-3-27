7526c5ef0093f97f5400c426f810a3ec29bb428d
Helle van den Broek <hvbroek@stud.ntnu.no>
2018-10-09 18:06:23 +0200

    Added content to the home-page

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons";
 
 
 export default class HomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>
-          Homepage
-        </Text>
+
+      <View style={styles.container}>
+
+        <View style={styles.iconStyle}>
+          <Icon name="ios-home" color="green" size={100}/>
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.welcome}>
+            Welcome to The Motivation App
+          </Text>
+          <Text style={styles.textStyle}>
+            This is your Home page. Use the tab bar at the bottom of the screen to navigate.
+          </Text>
+        </View>
+
       </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  iconStyle: {
+
+  },
+  textContainer: {
+    alignItems: "center",
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  welcome: {
+    fontSize: 30,
+    color: "green",
+    textAlign: 'center',
+  },
+  textStyle: {
+    fontSize: 20,
+    color: "grey",
+    textAlign: 'center',
+  }
+
+});
