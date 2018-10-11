c3c535ba8a236895011d04e32389b18288dd8648
Artorp <Artorp@users.noreply.github.com>
2018-10-11 13:19:57 +0200

    Add tests for static helper methods

--- components/Calendar/CalendarScreen.test.js
+++ components/Calendar/CalendarScreen.test.js
@@ -8,3 +8,24 @@ it("renders correctly", () => {
   const result = shallow.getRenderOutput();
   expect(result.props.children).toBeFalsy();
 });
+
+describe("test static helper methods", () => {
+  test("timeToString", () => {
+    const startTime = "2018-10-11";
+    const date = new Date(startTime);
+    const result = CalendarScreen.timeToString(date.getTime());
+    expect(result).toBe(startTime);
+  });
+  test("rowHasChanged equals returns false", () => {
+    const item1 = {name: "itemName", note: "itemNote"};
+    const item2 = {name: "itemName", note: "itemNote"};
+    const result = CalendarScreen.rowHasChanged(item1, item2);
+    expect(result).toBeFalsy();
+  });
+  test("rowHasChanged different returns true", () => {
+    const item1 = {name: "itemName", note: "itemNote"};
+    const item2 = {name: "itemName", note: "itemNote has changed"};
+    const result = CalendarScreen.rowHasChanged(item1, item2);
+    expect(result).toBeTruthy();
+  });
+});
