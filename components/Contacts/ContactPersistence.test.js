69b726cdd3b2e8a6ef6a0e65fb7563daa7605475
Helle van den Broek <hvbroek@stud.ntnu.no>
2018-10-16 16:23:30 +0200

    Asyncstorage is now tested in contactPersistence

--- components/Contacts/ContactPersistence.test.js
+++ components/Contacts/ContactPersistence.test.js
@@ -5,7 +5,6 @@ const AsyncStorage = new MockStorage(storageCache);
 jest.setMock("AsyncStorage", AsyncStorage);
 
 
-
 import React from 'react';
 
 import ContactPersistence from './ContactPersistence';
@@ -16,10 +15,11 @@ describe("Test ContactPersistence", () => {
 
   test("allIds first should return empty object", async (done) => {
     const result = await ContactPersistence.allIds();
-    expect(result).toEqual({});
+    const all = await  ContactPersistence.getContactsByIds(result);
+    expect(result).toEqual([]);
+    expect(all).toEqual([]);
     done();
   });
-
 
   test("saveContact", async (done) => {
     const contact = {
@@ -31,14 +31,15 @@ describe("Test ContactPersistence", () => {
     await ContactPersistence.saveContact(contact);
     const result = await ContactPersistence.allIds();
     expect(result).toBeDefined();
-    const outContact = result[0];
+    const contacts = await ContactPersistence.getContactsByIds(result);
+    const outContact = contacts[0];
     expect(outContact.name).toEqual(contact.name);
     expect(outContact.number).toEqual(contact.number);
     expect(outContact.key).toEqual(contact.key);
     done();
   });
 
-  test("deleteConctact should delete contact", async (done) => {
+  test("deleteContact should delete contact", async (done) => {
     const contact = {
       name: 'mama',
       number: '1234',
@@ -46,11 +47,18 @@ describe("Test ContactPersistence", () => {
       color: 'green'
     };
     await ContactPersistence.saveContact(contact);
-    const resultWithContact = await ContactPersistence.allIds();
+    const resultWithContactId = await ContactPersistence.allIds();
+    const resultWithContact = await ContactPersistence.getContactsByIds(resultWithContactId);
     expect(resultWithContact).toBeDefined();
     expect(Object.keys(resultWithContact).length).toEqual(1);
+
+    //Throws error msg: No such key. Need to fix, but test works
+    console.log("før");
     await ContactPersistence.deleteContact(contact);
-    const result = await ContactPersistence.allIds();
+    console.log("etter");
+
+    const resultId = await ContactPersistence.allIds();
+    const result = await ContactPersistence.getContactsByIds(resultId);
     expect(result).toBeDefined();
     expect(Object.keys(result).length).toEqual(0);
     done();
